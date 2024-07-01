a796d479606f956396730171df721afb838a1e00
Hikoyu <azrianpintar@gmail.com>
2024-07-01 16:11:08 +0700

    add transition

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-const tra = document.getElementById("transition")
+function Start() {
+  const tra = document.getElementById("transition");
+  tra.style.display = "block";
+  tra.style.animation = "transition 4s ease-in";
+  setTimeout(()=>{
+    tra.style.width="100%"
+    tra.style.height="100vh"
+    tra.style.borderRadius = "0"
+  }, 4000)
+}
