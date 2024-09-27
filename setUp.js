67ca21932c40f70d8ad452149ec3c451b0dd39be
Hikoyu <azrianpintar@gmail.com>
2024-09-27 20:42:33 +0700

    fix: #C10 - add setUp website (is like information)

---
+++ setUp.js
@@ -0,0 +1,10 @@
+// global variabel
+const version = 'v2.2'
+const language = ['indonsia', 'inggris']
+
+// getting
+const vers = document.getElementById('version')
+const lgg = document.getElementById('laguange')
+
+// implementasi
+vers.innerText = version
